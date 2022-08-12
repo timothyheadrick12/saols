06201328b9ba6015c3ed71100efeec6150d60847
Timothy Headrick <timothyheadrick12@gmail.com>
2022-08-12 17:11:43 -0500

    WIP(): Adding Sword Skill Calcs

--- scripts/damage_calc.py
+++ scripts/damage_calc.py
@@ -33,9 +33,16 @@ def calc_spd(handling, agi):
 def calc_basic_atk_from_lvl(original_atk, atk_increase_per_lvl, lvl):
     return original_atk + (lvl - 1) * atk_increase_per_lvl
 
-
 def calc_basic_atk_spam_dmg(spd, atk, atk_cost):
     return floor(spd / atk_cost) * atk
+
+def calc_best_atk_combination_dmg(spd, atk, atk_cost, handling, sword_skills, cur_dmg=0, combo_dmgs=[]):
+    for sword_skill in [*sword_skills, {"name": "bsc","lvl": 1,"atk_mul": 1,"num_hits": 1,"min_handling": 0,"spd_cost": atk_cost}]:
+        if(sword_skill['spd_cost'] <= spd and sword_skill['min_handling'] <= handling):
+            calc_best_atk_combination_dmg(spd - sword_skill['spd_cost'], atk, atk_cost, handling, sword_skills, cur_dmg + atk * sword_skill['atk_mul'] * sword_skill['num_hits'], combo_dmgs)
+        else:
+            return combo_dmgs.append(cur_dmg)
+
 
 
 def parse_weapon_csv(file_location):
@@ -69,27 +76,22 @@ def parse_sword_skill_csv(file_location):
     return sword_skills
 
 
-def main():
-    par_weapons = parse_weapon_csv(
-        "../docs/par_weapon_stats.csv",
-    )
-    sword_skills = parse_sword_skill_csv("../docs/sword_skills.csv")
-    print(sword_skills)
-    max_damage_per_lvl = {}
+def calc_all_weapon_par_dmg_across_lvls(par_weapons):
     for lvl in range(1, 101):
+        print(f'LVL {lvl}---------------------------------------')
         builds = []
-        top_3_builds = {1: ["", 0, 0, 0], 2: ["", 0, 0, 0], 3: ["", 0, 0, 0]}
         available_pts = (lvl - 1) * POINTS_PER_LVL
         weapons_unlocked = [
             weapon for weapon in par_weapons if lvl >= weapon["unlock_lvl"]
         ]
         # total_calcs = available_pts * len(weapons_unlocked) + len(weapons_unlocked)
         for weapon in weapons_unlocked:
+            weight = calc_weight_from_lvl(weapon["weight"], lvl)
             for agi in range(available_pts + 1):
                 cur_agi = agi + STARTING_AGI
                 cur_str = available_pts - agi + STARTING_STR
                 handling = calc_handling(
-                    calc_weight_from_lvl(weapon["weight"], lvl), cur_str
+                    weight, cur_str
                 )
                 spd = calc_spd(handling, cur_agi)
                 cur_dmg = calc_basic_atk_spam_dmg(
@@ -126,7 +128,6 @@ def main():
                                 },
                             )
                             break
-        max_damage_per_lvl[lvl] = top_3_builds
         named_weapons_added = []
         new_builds = []
         total_dmg_of_top_builds = 0
@@ -143,6 +144,40 @@ def main():
         print(
             f"AVG DMG ACROSS ALL TOP WEAPON BUILDS: {round(total_dmg_of_top_builds/len(new_builds))}"
         )
+
+def calc_weapon_dmg_across_lvls(provided_weapon, sword_skills):
+    for lvl in range(1, 101):
+        builds = []
+        available_pts = (lvl - 1) * POINTS_PER_LVL
+        weapons_unlocked = [
+            weapon for weapon in [provided_weapon] if lvl >= weapon["unlock_lvl"]
+        ]
+        # total_calcs = available_pts * len(weapons_unlocked) + len(weapons_unlocked)
+        for weapon in weapons_unlocked:
+            weight = calc_weight_from_lvl(weapon["weight"], lvl)
+            for agi in range(available_pts + 1):
+                cur_agi = agi + STARTING_AGI
+                cur_str = available_pts - agi + STARTING_STR
+                handling = calc_handling(
+                    weight, cur_str
+                )
+                spd = calc_spd(handling, cur_agi)
+                cur_dmg = calc_basic_atk_from_lvl(weapon["base_dmg"], weapon["dmg_pl"], lvl)
+               
+                cur_dmg = calc_best_atk_combination_dmg(spd, cur_dmg, weapon['basic_atk_cost'], handling,sword_skills)
+                if handling < HANDLING_CUTOFF:
+                    cur_dmg = calc_encumberment_dmg(cur_dmg, handling)
+                # cur_dmg = round(cur_dmg)
+
+def main():
+    par_weapons = parse_weapon_csv(
+        "../docs/par_weapon_stats.csv",
+    )
+    sword_skills = parse_sword_skill_csv("../docs/sword_skills_min.csv")
+    print(sword_skills)
+    #calc_all_weapon_par_dmg_across_lvls(par_weapons)   
+    calc_weapon_dmg_across_lvls(par_weapons[0], [])
+
 
 
 if __name__ == "__main__":
